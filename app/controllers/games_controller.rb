efb321584cee926e0e2f72ff3dee08dcc9fad8dd
Joana Areas <jo_areas@hotmail.com>
2022-08-20 14:32:51 -0300

    longest words app - first version

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,43 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+
+  def home
+  end
+
+  def new
+    @letters = (1..10).map { rand(65..90).chr }
+  end
+
+  def score
+    @word = params[:user_answer].upcase
+    @grid = params[:letters].split
+    @message =  case check_word
+                when 1 then 'Sorry, but word can`t be built out of the grid'
+                when 2 then 'Sorry but word does not seem to be a valid English word...'
+                else
+                  'Congratulations!!'
+                end
+  end
+
+  private
+
+  def in_grid?
+    @word.chars.all? { |letter| @word.count(letter) <= @grid.count(letter) }
+  end
+
+  def english_word?
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
+    hash = JSON.parse(response.read)
+    hash['found']
+  end
+
+  def check_word
+    if !in_grid?
+      1
+    elsif !english_word?
+      2
+    end
+  end
+end
